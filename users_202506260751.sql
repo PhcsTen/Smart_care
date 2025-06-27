d3983659cca2f773a6e427b758ef0e36f12ef866
PhcsTen <133624290+PhcsTen@users.noreply.github.com>
2025-06-27 20:46:53 +0700

    Update users_202506260751.sql

--- users_202506260751.sql
+++ users_202506260751.sql
@@ -1,2 +1,2 @@
-INSERT INTO smart_kids.users (user_name,password,email,created_date,created_by,updated_date,updated_by,user_status) VALUES
-	 ('admin','$2b$12$.LwkoScEUCM66micm4c51ukttEaf/aRBIFwmIgrzebHp3WHr3CG1.','phcstenkku@gmail.com',NULL,NULL,NULL,NULL,1);
+INSERT INTO smart_kids.users (user_name,password,email,created_date,created_by,updated_date,updated_by) VALUES
+	 ('admin','$2b$12$.LwkoScEUCM66micm4c51ukttEaf/aRBIFwmIgrzebHp3WHr3CG1.','phcstenkku@gmail.com',NULL,NULL,NULL,NULL);
