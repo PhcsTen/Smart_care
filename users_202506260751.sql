bc4928aa4d1a1cedfae9b46c3c9c76ef713df402
PhcsTen <133624290+PhcsTen@users.noreply.github.com>
2025-07-10 18:02:20 +0700

    Delete users_202506260751.sql

--- users_202506260751.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO smart_kids.users (user_name,password,email,created_date,created_by,updated_date,updated_by) VALUES
-	 ('admin','$2b$12$.LwkoScEUCM66micm4c51ukttEaf/aRBIFwmIgrzebHp3WHr3CG1.','phcstenkku@gmail.com',NULL,NULL,NULL,NULL);
